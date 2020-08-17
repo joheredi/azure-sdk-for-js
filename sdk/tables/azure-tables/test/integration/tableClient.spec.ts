21826e699c640818d6a9af595ca1c27833cfb845
Jose Manuel Heredia Hidalgo <joheredi@microsoft.com>
2020-08-17 21:31:41 +0000

    Format

--- sdk/tables/azure-tables/test/integration/tableClient.spec.ts
+++ sdk/tables/azure-tables/test/integration/tableClient.spec.ts
@@ -57,7 +57,7 @@ describe("TableClient", () => {
         name: "testEntity"
       };
       const result = await client.createEntity(testEntity, {});
-  
+
       assert.equal(result.PartitionKey, testEntity.PartitionKey);
       assert.equal(result.RowKey, testEntity.RowKey);
       assert.equal(result.name, testEntity.name);
@@ -71,56 +71,56 @@ describe("TableClient", () => {
         testDate
       };
       const result = await client.createEntity(testEntity, {});
-  
+
       assert.equal(result.PartitionKey, testEntity.PartitionKey);
       assert.equal(result.RowKey, testEntity.RowKey);
       assert.deepEqual(result.testDate, testDate);
     });
 
     it("should createEntity with Guid", async () => {
-      const testGuid: Edm<"Guid"> = {value: "cf8ef051-1b7d-4e93-a1e5-a3944d7e441c", type: "Guid" };
+      const testGuid: Edm<"Guid"> = {  value: "cf8ef051-1b7d-4e93-a1e5-a3944d7e441c", type: "Guid" };
       const testEntity: TableEntity<{ testGuid: Edm<"Guid"> }> = {
         PartitionKey: "P2",
         RowKey: "R2",
         testGuid
       };
       const result = await client.createEntity(testEntity, {});
-  
+
       assert.equal(result.PartitionKey, testEntity.PartitionKey);
       assert.equal(result.RowKey, testEntity.RowKey);
       assert.deepEqual(result.testGuid, testGuid);
     });
 
     it("should createEntity with Int64", async () => {
-      const testInt64: Edm<"Int64"> = {value: "12345543221", type: "Int64" };
+      const testInt64: Edm<"Int64"> = {  value: "12345543221", type: "Int64" };
       const testEntity: TableEntity<{ testInt64: Edm<"Int64"> }> = {
         PartitionKey: "P2",
         RowKey: "R2",
         testInt64
       };
       const result = await client.createEntity(testEntity, {});
-  
+
       assert.equal(result.PartitionKey, testEntity.PartitionKey);
       assert.equal(result.RowKey, testEntity.RowKey);
       assert.deepEqual(result.testInt64, testInt64);
     });
 
     it("should createEntity with Int32", async () => {
-      const testInt32: Edm<"Int32"> = {value: 123, type: "Int32" };
+      const testInt32: Edm<"Int32"> = {  value: 123, type: "Int32" };
       const testEntity: TableEntity<{ testInt32: Edm<"Int32"> }> = {
         PartitionKey: "P2",
         RowKey: "R2",
         testInt32
       };
       const result = await client.createEntity(testEntity, {});
-  
+
       assert.equal(result.PartitionKey, testEntity.PartitionKey);
       assert.equal(result.RowKey, testEntity.RowKey);
       assert.deepEqual(result.testInt32, testInt32);
     });
 
     it("should createEntity with Boolean", async () => {
-      const testBoolean: Edm<"Boolean"> = {value: true, type: "Boolean" };
+      const testBoolean: Edm<"Boolean"> = {  value: true, type: "Boolean" };
       // Check this API interaction!
       const testEntity: TableEntity<{ testBoolean: Edm<"Boolean"> | boolean }> = {
         PartitionKey: "P2",
@@ -128,7 +128,7 @@ describe("TableClient", () => {
         testBoolean
       };
       const result = await client.createEntity(testEntity, {});
-  
+
       assert.equal(result.PartitionKey, testEntity.PartitionKey);
       assert.equal(result.RowKey, testEntity.RowKey);
       assert.equal(result.testBoolean, true);
@@ -136,7 +136,7 @@ describe("TableClient", () => {
 
     it("should createEntity with DateTime", async () => {
       const testDate = new Date(2020, 8, 17);
-      const testDateTime: Edm<"DateTime"> = {value: testDate, type: "DateTime" };
+      const testDateTime: Edm<"DateTime"> = {  value: testDate, type: "DateTime" };
       // Check this API interaction!
       const testEntity: TableEntity<{ testDateTime: Edm<"DateTime"> | Date }> = {
         PartitionKey: "P2",
@@ -144,10 +144,10 @@ describe("TableClient", () => {
         testDateTime
       };
       const result = await client.createEntity(testEntity, {});
-  
-      assert.equal(result.PartitionKey, testEntity.PartitionKey);
+
+    assert.equal(result.PartitionKey, testEntity.PartitionKey);
       assert.equal(result.RowKey, testEntity.RowKey);
       assert.equal(result.testDateTime, testDate);
     });
-  }) 
+  });; 
 });
